337e19aaaa3128ed4e3c9777a3017307f538002d
xiaoyegecode <85229018+xiaoyegecode@users.noreply.github.com>
2021-07-12 14:58:27 +0800

    Add files via upload

--- lodash/xiaoyegecode-lodash.js
+++ lodash/xiaoyegecode-lodash.js
@@ -1,1 +1,27 @@
-var
+var xiaoyegecode = function () {
+
+  function chunk(array, size = 2) {
+    if (array.lenght == 0) {
+      return []
+    }
+    let length = array.length
+    let res = []
+    let index = 0
+    while (index < length) {
+      res.push(array.slice(index, index + size))
+      index += size
+    }
+    return res
+
+  }
+
+  function compact() {
+
+  }
+
+
+  return {
+    chunk: chunk,
+    compact: compact,
+  }
+}
